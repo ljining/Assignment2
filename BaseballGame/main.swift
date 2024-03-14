726b644be23d36c26b790ddea8230e25f51ba5e0
ljining <161561452+ljining@users.noreply.github.com>
2024-03-14 15:56:38 +0900

    Lv2

--- BaseballGame/main.swift
+++ BaseballGame/main.swift
@@ -11,14 +11,29 @@ import Foundation
 class NumberBaseball {
     
     func start() {
+        while true{
+            print("< 게임을 시작합니다 >")
+            var input: String = ""         //값 입력 받기
+            
+            switch input {                 //경우 나누기
+            case input != input.contains { $0.isNumber }:
+                print("올바르지 않은 입력값입니다")
+            
+            case input.contains ("0"):
+                print("올바르지 않은 입력값입니다")
+            
+            case input.count != 3:
+                print("올바르지 않은 입력값입니다")
+            
+            else
+            
+            
+        
+    }
+    
+    func makeAnswer() {
         var array = Array(1...9)
         array.shuffle()
         array.removeSubrange(3...8)
-
-        print(array)
     }
-    
 }
-
-let game = NumberBaseball()
-game.start()
